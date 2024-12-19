5efc4486c24e30dcfe2fbb6ade12100afdc9811b
Anirudh <anirudh.choudary@unblu.com>
2024-12-20 00:03:46 +0100

    Day 18

---
+++ day18/d18p2.py
@@ -0,0 +1,74 @@
+from collections import deque
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        coordinates = []
+        for line in file:
+            x, y = map(int, line.strip().split(','))
+            coordinates.append((x, y))
+    return coordinates
+
+def create_grid(coordinates, G, byteCtr):
+    grid = [['.' for _ in range(G+1)] for _ in range(G+1)]
+    for array in grid:
+        print(''.join(array))
+    for i, (x, y) in enumerate(coordinates[:byteCtr]):
+        if 0 <= x <= G and 0 <= y <= G:
+            grid[y][x] = '#'
+    print("============")
+    for array in grid:
+        print(''.join(array))
+    return grid
+
+def shortest_path(grid, G):
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    start, end = (0, 0), (G, G)
+    queue = deque([(start[0], start[1], 0)])
+    visited = set()
+
+    while queue:
+        x, y, steps = queue.popleft()
+        if (x, y) == end:
+            return steps
+
+        if (x, y) in visited:
+            continue
+
+        visited.add((x, y))
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx <= G and 0 <= ny <= G and grid[ny][nx] == '.' and (nx, ny) not in visited:
+                queue.append((nx, ny, steps + 1))
+
+    return -1
+
+def check_blocking_positions(file_path):
+    G = 70
+    byteCtr = 1024
+
+    coordinates = read_file(file_path)
+    grid = create_grid(coordinates, G, byteCtr)
+
+    for i, (x, y) in enumerate(coordinates[byteCtr:]):
+        if 0 <= x <= G and 0 <= y <= G:
+            grid[y][x] = '#'
+
+        steps = shortest_path(grid, G)
+
+        if steps == -1:
+            print(f"Path is blocked by position: ({x}, {y})")
+        print("============")
+        for array in grid:
+            print(''.join(array))
+        return
+
+
+    print("No blocking positions found. Path remains clear.")
+
+def main():
+    file_path = 'd18-input.txt'
+    check_blocking_positions(file_path)
+
+if __name__ == "__main__":
+    main()
